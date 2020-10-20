4daf7ab26718c21bac51cf7fd2ceafac3bdf19a9
zhtmr <zhtmr123@gmail.com>
2020-10-20 09:45:27 +0900

    1020

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-s1=pd.read_csv("monthdata.csv", names=['year','month','data'])
-data1=s1.set_index(['year','month']).sort_index()
-data2=data1.reset_index(inplace=False)
+# 위쪽 라벨
+data1 = pd.read_csv("monthdata.csv", names=['year','month','data'])
+# year, month 기반으로 인덱스, 정렬
+data2 = data1.set_index(['year','month']).sort_index()
+# 차트에 맞는 형식으로 바꾸기
+data3 = data2.reset_index(inplace=False)
+
 for i in range(4):
-    data3=data2.loc[0+(i*12):11+(i*12)]
-    plt.plot(data3['month'], data3['data'])
+    data4 = data3.loc[0+(i*12):11+(i*12)] # [0:11]2005.1~12월, [12:23]2006.1~12월, ...
+    print(data4)
+    plt.plot(data4['month'],data4['data'])
+
+# 범례
 plt.legend(['2005','2006','2007','2008'])
 plt.show()
